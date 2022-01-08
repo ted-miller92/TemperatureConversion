19f816138f227d53c63150900e7241c7ddcfb3e4
ted-miller92 <ted.miller92@gmail.com>
2022-01-07 17:01:41 -0800

    Created branch for ted, started basic logic of app

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+// checking that all fields are filled out/set
+if(isset($_POST['InputTemp']) && isset($_POST['InputUnit']) && isset($_POST['OutputUnit'])){
+    // checking that input temperature is a number
+    if(is_numeric($_POST['InputTemp']) == "integer"){
+        // echo '<span> Data is correct type, perform calculations</span>';
+        // Declare variables
+        $InputTemp = number_format($_POST['InputTemp'], 2);
+        $InputUnit = $_POST['InputUnit'];
+        $OutputUnit = $_POST['OutputUnit'];
+        $OutputTemp;
+
+        // calculations go here
+
+        // if InputUnit is F
+        if($InputUnit == 'F'){
+            if($OutputUnit == 'C'){ 
+                // convert F to C
+                $OutputTemp = number_format(($InputTemp - 32) * (5/9), 2);
+            }elseif($OutputUnit == 'K'){
+                // convert to K
+                $OutputTemp = number_format(($InputTemp - 32) * (5/9) + 273.15, 2);
+            }else{
+                // no conversion
+                $OutputTemp = number_format($InputTemp, 2);
+            }
+        }
+        // if InputUnit is C
+            
+            // convert to F
+            
+            // convert to K
+
+        // if InputUnit is K
+            
+            // convert to F
+
+            // convert to C
+        
+        // $OutputTemp $OutputUnit
+    }else{
+        // echo number_format($_POST['InputTemp'], 2);
+        
+        echo '<span>Data is incorrect type. Please enter a number</span>';
+    }
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form action="" method="POST">
+            <p>Temp to convert: <input type="text" name="InputTemp"/>
+                <select name="InputUnit">
+                    <option value="F" selected>F</option>
+                    <option value="C">C</option>
+                    <option value="K">K</option>
+                </select>
+
+            </p>
+            <p>Convert to:
+                <select name="OutputUnit">
+                    <option value="F">F</option>
+                    <option value="C">C</option>
+                    <option value="K">K</option>
+                </select>
+            </p>
+            <button type="submit">Convert</button>
+        </form>
+        <div id="results">
+            <p>Converted Temperature:<?php 
+            if(isset($OutputTemp)){
+                echo ''.$OutputTemp.' degrees '.$OutputUnit.'';
+            }
+            ?></p>
+        </div>
+    </body>
+</html>
